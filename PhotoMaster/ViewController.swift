a80141147e6d047e41ee5d378534a72fec4e3e9e
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-19 15:15:00 +0900

    完成

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,12 +8,126 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    //写真表示用
+    @IBOutlet var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //「カメラ」ボタンを押した時に呼ばれるメソッド
+    @IBAction func onTappedCameraButton() {
+        presentPickerController(sourceType: .camera)
+    }
+    //「アルバム」ボタンを押した時に呼ばれるメソッド
+    @IBAction func onTappedAlbumButton() {
+        presentPickerController(sourceType: .photoLibrary)
+        
+    }
+    
+    //カメラ、アルバムの呼び出しメソッド（カメラorアルバムのソースタイプが引数）
+    func presentPickerController(sourceType: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    //写真が選択された際に呼ばれるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey: Any]){
+        self.dismiss(animated: true, completion: nil)
+        //画像を出力
+        photoImageView.image = info[.originalImage]as? UIImage
+    }
+    //元の画像にテキストを合成するメソッド
+    func drawText(image: UIImage)-> UIImage{
+        
+        //テキストの内容の設定
+        let text = "LifeisTech!"
+        
+        let textFontAttributes = [
+        NSAttributedString.Key.font: UIFont(name:"Arial", size: 120)!,
+        NSAttributedString.Key.foregroundColor: UIColor.red
+        ]
+        //グラフィックコンテキスト生成編集を開始
+        UIGraphicsBeginImageContext(image.size)
+        
+        //読み込んだ写真を書き出す
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        //定義
+        let margin: CGFloat = 5.0 //余白
+        let textRect = CGRect(x: margin, y: margin, width: image.size.width - margin, height: image.size.height - margin)
+        
+        //textRectで指定した範囲にtextFontAttributesにしたがってtextを描き出す
+        text.draw(in: textRect, withAttributes: textFontAttributes)
+        
+        //グラフィックスコンテキストの画像を取得
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        //グラフィックコンテキストの編集を終了
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+        
+    }
+    //元の画像にイラストを合成するメソッド
+    func drawMaskImage(image: UIImage)-> UIImage{
+        
+        let maskImage = UIImage(named: "furo_ducky")!
+        
+        UIGraphicsBeginImageContext(image.size)
+    
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        let margin: CGFloat = 50.0 //余白
+        let maskRect = CGRect(x: image.size.width - maskImage.size.width - margin,
+                              y: image.size.height - maskImage.size.height - margin,
+                              width: maskImage.size.width - margin, height: maskImage.size.height - margin)
+        maskImage.draw(in: maskRect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    //「テキスト合成」ボタンを押した時に呼ばれるメソッド
+    @IBAction func onTappedTextButton() {
+        if photoImageView.image != nil {
+            photoImageView.image = drawText(image: photoImageView.image!)
+        }else {
+            print("画像がありません")
+        }
+    }
+    //「イラスト合成」ボタンを押した時に呼ばれるメソッド
+    @IBAction func onTappedIllustButton() {
+        if photoImageView.image != nil {
+            photoImageView.image = drawMaskImage(image: photoImageView.image!)
+        }else {
+            print("画像がありません")
+        }
+    }
+    
+    // 「アップロード」ボタンを押した時に呼ばれるメソッド
+    @IBAction func onTappedUploadButton() {
+        if photoImageView.image != nil {
+            //共有するアイテムを設定
+            let activityVC = UIActivityViewController(activityItems: [photoImageView.image!, "#PhotoMaster"], applicationActivities: nil)
+            self.present(activityVC, animated: true, completion: nil)
+        } else {
+            print("画像がありません")
+        }
+    }
+        
+    
+        
+        
+        
+    
 
 
 }
